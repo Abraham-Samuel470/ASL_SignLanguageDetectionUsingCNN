76c2bcabd22e64a264ebf599aeeea09d7eef4fb3
Abraham Samuel <samuak2004@gmail.com>
2025-01-13 13:38:02 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-print("Keras version:", tf.keras.__version__)
-
-# Simple test
-model = tf.keras.Sequential([tf.keras.layers.Dense(1, input_shape=(1,))])
-print("Model created successfully")
